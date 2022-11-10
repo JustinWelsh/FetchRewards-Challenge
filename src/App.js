1fd36e8f0cbc1a7c492a2366d06c8be62f5a2e79
Justin Welsh <justinwelsh33@gmail.com>
2022-11-10 17:01:20 -0600

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+        <h3> Lets go for a <FaBeer />? </h3>
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
