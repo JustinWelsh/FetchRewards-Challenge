35785e0065da7aed50f8f2bb7aeaaf43d1409d05
Justin Welsh <justinwelsh33@gmail.com>
2022-11-11 12:39:02 -0600

    clean up

--- src/components/BackgroundImg.jsx
+++ src/components/BackgroundImg.jsx
@@ -2,17 +2,17 @@ const BackgroundImg = () => (
   <>
     <img
       className="absolute w-full"
-      src={`${process.env.PUBLIC_URL}/images/layered-waves-haikei.svg`}
+      src={`${process.env.PUBLIC_URL}/images/background/layered-waves-haikei.svg`}
       alt="background"
     />
     <img
       className="absolute w-full"
-      src={`${process.env.PUBLIC_URL}/images/star-scatter-haikei-yellow.svg`}
+      src={`${process.env.PUBLIC_URL}/images/background/star-scatter-haikei-yellow.svg`}
       alt="background"
     />
     <img
       className="absolute w-full"
-      src={`${process.env.PUBLIC_URL}/images/star-scatter-haikei-purple.svg`}
+      src={`${process.env.PUBLIC_URL}/images/background/star-scatter-haikei-purple.svg`}
       alt="background"
     />
   </>
