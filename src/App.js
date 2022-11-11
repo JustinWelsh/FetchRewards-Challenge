556ceb91a2523470dbef02ad8031e3b2321d8cde
Justin Welsh <justinwelsh33@gmail.com>
2022-11-11 01:09:24 -0600

    added background

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img
+          className="absolute w-full"
+          src={`${process.env.PUBLIC_URL}/images/layered-waves-haikei.svg`}
+          alt="background"
+        />
+        <img
+          className="absolute w-full"
+          src={`${process.env.PUBLIC_URL}/images/symbol-scatter-haikei.svg`}
+          alt="background"
+        />
         <Form />
       </header>
     </div>
