42262424b2d4b5f1132820beb30d9a3d14a9ae14
Justin Welsh <justinwelsh33@gmail.com>
2022-11-16 13:45:15 -0600

    hooked up success-modal

--- src/components/form/UserCreationForm.jsx
+++ src/components/form/UserCreationForm.jsx
@@ -207,6 +207,28 @@ const UserCreationForm = (props) => {
           <Button disabled={isSubmitDisabled} onClick={handleSubmit}>
             Submit
           </Button>
+
+          {/* The button to open modal */}
+          <label
+          disabled={isSubmitDisabled}
+          htmlFor="my-modal-6"
+          onClick={handleSubmit}
+          className="btn rounded-full bg-[#300C38] hover:bg-[#880174] border-none text-white shadow-md disabled:bg-[#300C38] disabled:opacity-50">open modal
+          </label>
+
+          {/* Put this part before </body> tag */}
+          <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+          <div className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Congratulations, user was successfully created!</h3>
+              <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+              <div className="modal-action">
+                <label htmlFor="my-modal-6" className="btn">Ok</label>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </>
