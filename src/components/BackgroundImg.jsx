69fde0afd763cd43a232df1d596225b56dbaa2ea
Justin Welsh <justinwelsh33@gmail.com>
2022-11-16 22:23:37 -0600

    mobile responsive classes added

--- src/components/BackgroundImg.jsx
+++ src/components/BackgroundImg.jsx
@@ -1,17 +1,17 @@
 const BackgroundImg = () => (
   <>
     <img
-      className="absolute w-full"
+      className="absolute w-full bottom-0"
       src={`${process.env.PUBLIC_URL}/images/background/layered-waves-haikei.svg`}
       alt="background"
     />
     <img
-      className="absolute w-full"
+      className="absolute w-full lg:bottom-0"
       src={`${process.env.PUBLIC_URL}/images/background/star-scatter-haikei-yellow.svg`}
       alt="background"
     />
     <img
-      className="absolute w-full"
+      className="absolute w-full lg:bottom-0"
       src={`${process.env.PUBLIC_URL}/images/background/star-scatter-haikei-purple.svg`}
       alt="background"
     />
--- src/components/form/UserCreationForm.jsx
+++ src/components/form/UserCreationForm.jsx
@@ -139,9 +139,9 @@ const UserCreationForm = (props) => {
   
   return (
     <>
-      <div className="card flex-shrink-0 w-full shadow-2xl bg-white pt-7
+      <div className="card flex-shrink-0 w-full shadow-2xl bg-white pt-3 sm:pt-7
       max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
-        <h1 className="text-4xl font-bold text-[#300C38] text-center">
+        <h1 className="text-3xl sm:text-4xl font-bold text-[#300C38] text-center">
           User Creation Form
         </h1>
         <div className="card-body text-[#300C38]">
